724c655a89629d38e7b70446ff589ada22119b20
kellan-dev <174945686+kellan-dev@users.noreply.github.com>
2024-12-13 11:57:15 -0500

    Styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-lg">Home Page</h2>
-
-      <Link href={paths.tickets()} className="underline">
-        Tickets
-      </Link>
+    <div className="flex flex-1 flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-muted-foreground text-sm">
+          Your home place to start
+        </p>
+      </div>
+      <div className="flex flex-1 flex-col items-center">
+        <Link href={paths.tickets()} className="underline">
+          Tickets
+        </Link>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,15 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        "fade-in-from-top": {
+          "0%": { opacity: "0", transform: "translateY(-16px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in-from-top": "fade-in-from-top 0.5s ease-out",
+      },
     },
   },
   plugins: [],
