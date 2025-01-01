6e3ca53f325604813e7d6c80b2569d5586547b59
kellan-dev <174945686+kellan-dev@users.noreply.github.com>
2025-01-01 14:51:29 -0500

    Folder Naming Conventions

--- app/(protected)/account/password/page.tsx
+++ app/(protected)/account/password/page.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import Heading from "@/components/heading";
-import AccountTabs from "@/features/account/components/account-tabs";
+import AccountTabs from "@/app/(protected)/account/_components/account-tabs";
 
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading
-        title="Profile"
-        description="Manage your profile"
+        title="Password"
+        description="Keep your account secure"
         tabs={<AccountTabs />}
       />
     </div>
--- app/(protected)/account/profile/page.tsx
+++ app/(protected)/account/profile/page.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import Heading from "@/components/heading";
-import AccountTabs from "@/features/account/components/account-tabs";
+import AccountTabs from "@/app/(protected)/account/_components/account-tabs";
 
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading
-        title="Password"
-        description="Keep your account secure"
+        title="Profile"
+        description="Manage your profile"
         tabs={<AccountTabs />}
       />
     </div>
